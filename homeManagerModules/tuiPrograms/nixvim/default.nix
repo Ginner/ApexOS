10846d47553c9550cc746b9954c4af417993f46c
Ginner <26798615+Ginner@users.noreply.github.com>
2026-05-27 22:23:00 +0200

    Suppress build-time evaluation warning

--- homeManagerModules/tuiPrograms/nixvim/default.nix
+++ homeManagerModules/tuiPrograms/nixvim/default.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 let
   cfg = config.myHomeModules.tuiPrograms.nixvim;
@@ -21,47 +27,48 @@ in
   ];
 
   config = lib.mkIf cfg.enable {
-  programs.nixvim = {
-    enable = true;
+    programs.nixvim = {
+      enable = true;
+      nixpkgs.source = pkgs.path;
 
-    # colorschemes.gruvbox.enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    globals.mapleader = " ";
-    clipboard = {
-      register = "unnamed";
-      providers.wl-copy.enable = true;
+      # colorschemes.gruvbox.enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      globals.mapleader = " ";
+      clipboard = {
+        register = "unnamed";
+        providers.wl-copy.enable = true;
+      };
+      plugins = {
+        telescope.enable = true;
+        web-devicons.enable = true;
+        vim-surround.enable = true;
+        lastplace.enable = true;
+      };
     };
-    plugins = {
-      telescope.enable = true;
-      web-devicons.enable = true;
-      vim-surround.enable = true;
-      lastplace.enable = true;
+    home.file = {
+      ".config/nvim/after/queries/markdown/highlights.scm".text = ''
+        ;; extends
+        (list_item [
+          (list_marker_plus)
+          (list_marker_minus)
+          (list_marker_star)
+          (list_marker_dot)
+          (list_marker_parenthesis)
+        ] @conceal [
+          (task_list_marker_checked)
+          (task_list_marker_unchecked)
+        ](#set! conceal ""))
+        ((task_list_marker_checked) @my_checked (#set! conceal "󰄵"))
+        ((task_list_marker_unchecked) @my_unchecked (#set! conceal "󰄱"))
+      '';
+      ".config/nvim/after/queries/markdown_inline/highlights.scm".text = ''
+        ;; extends
+        (shortcut_link (link_text) @my_partial (#lua-match? @my_partial "^/$") (#offset! @my_partial 0 -3 0 0)(#set! conceal "󱗝"))
+        (shortcut_link (link_text) @my_wont_do (#lua-match? @my_wont_do "^-$") (#offset! @my_wont_do 0 -3 0 0)(#set! conceal "󰅘"))
+      '';
     };
-  };
-  home.file = {
-    ".config/nvim/after/queries/markdown/highlights.scm".text = ''
-      ;; extends
-      (list_item [
-        (list_marker_plus)
-        (list_marker_minus)
-        (list_marker_star)
-        (list_marker_dot)
-        (list_marker_parenthesis)
-      ] @conceal [
-        (task_list_marker_checked)
-        (task_list_marker_unchecked)
-      ](#set! conceal ""))
-      ((task_list_marker_checked) @my_checked (#set! conceal "󰄵"))
-      ((task_list_marker_unchecked) @my_unchecked (#set! conceal "󰄱"))
-    '';
-    ".config/nvim/after/queries/markdown_inline/highlights.scm".text = ''
-      ;; extends
-      (shortcut_link (link_text) @my_partial (#lua-match? @my_partial "^/$") (#offset! @my_partial 0 -3 0 0)(#set! conceal "󱗝"))
-      (shortcut_link (link_text) @my_wont_do (#lua-match? @my_wont_do "^-$") (#offset! @my_wont_do 0 -3 0 0)(#set! conceal "󰅘"))
-    '';
-  };
   };
 }
