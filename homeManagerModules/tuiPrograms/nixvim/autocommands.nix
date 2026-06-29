5ba4623bfe18146d0d1532667e66f872cb1b9bfc
Ginner <26798615+Ginner@users.noreply.github.com>
2026-06-29 09:49:08 +0200

    Programming language specific settings should be per project

--- homeManagerModules/tuiPrograms/nixvim/autocommands.nix
+++ homeManagerModules/tuiPrograms/nixvim/autocommands.nix
@@ -5,7 +5,6 @@
     programs.nixvim.autoGroups = {
       lastplacegrp.clear = true;
       mymdgrp.clear = true;
-      pygrp.clear = true;
       wikigrp.clear = true;
     };
     programs.nixvim.autoCmd = [
@@ -36,13 +35,6 @@
         event = [ "BufLeave" ];
         pattern = [ "markdown" "*.md" "*.markdown" ];
         group = "mymdgrp";
-      }
-      {
-        desc = "Use Python indentation and line length defaults";
-        command = "setlocal tabstop=4 softtabstop=4 shiftwidth=4 textwidth=119 colorcolumn=120 expandtab autoindent fileformat=unix";
-        event = [ "BufRead" "BufNewFile" ];
-        pattern = [ "*.py" ];
-        group = "pygrp";
       }
       {
         desc = "Vertically center the buffer when entering insert mode";
